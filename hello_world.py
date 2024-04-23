80d9b617c43a1bc07233a34c2c7e29c72fab8b90
ronaks2309 <ronak.iitk@gmail.com>
2024-04-23 10:05:07 -0700

    git ignore file test

--- hello_world.py
+++ hello_world.py
@@ -6,4 +6,4 @@ st.write("db_username: ", st.secrets["OPENAI_API_KEY"])
 st.write("Has environment variables been set: ",
          os.environ["APP_PASSWORD"])
 
-#st.warning('This is a warning', icon="⚠️")
+st.warning('This is a warning', icon="⚠️")
